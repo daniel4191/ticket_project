432308b9219f2c9e788406b9f05fcedb4ad39a93
daniel4191 <daniell8282@naver.com>
2026-03-14 10:07:36 +0900

    java project started

---
+++ src/test/java/com/daniel/ticket_reservation/TicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.ticket_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
